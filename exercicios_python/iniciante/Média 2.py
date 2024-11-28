aafe02411af2827b9d86fd3bacf7dce6eff53d86
Richard Valente <richardvalente20052005@gmail.com>
2024-11-28 17:45:28 -0300

    exercicios do beecrowd

---
+++ exercicios_python/iniciante/Média 2.py
@@ -0,0 +1,35 @@
+'''
+Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+
+Entrada
+O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
+
+Saída
+Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+Exemplos de Entrada	Exemplos de Saída
+5.0
+6.0
+7.0
+
+MEDIA = 6.3
+
+5.0
+10.0
+10.0
+
+MEDIA = 9.0
+
+10.0
+10.0
+5.0
+
+MEDIA = 7.5
+'''
+
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota_final = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10
+
+print(f"MEDIA = {nota_final:.1f}")
---
+++ exercicios_python/iniciante/Teste de Seleção 1.py
@@ -0,0 +1,30 @@
+'''
+Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos", senão escrever "Valores nao aceitos".
+
+Entrada
+Quatro números inteiros A, B, C e D.
+
+Saída
+Mostre a respectiva mensagem após a validação dos valores.
+
+Exemplo de Entrada	Exemplo de Saída
+5 6 7 8
+
+Valores nao aceitos
+
+2 3 2 6
+
+Valores aceitos
+'''
+
+numeros = input()
+separa_numeros = [int(n) for n in numeros.split()]
+a = separa_numeros[0]
+b = separa_numeros[1]
+c = separa_numeros[2]
+d = separa_numeros[3]
+
+if b > c and d > a and c + d > a + b and c > 0 and d > 0 and a % 2 == 0:
+    print("Valores aceitos")
+else:
+    print("Valores nao aceitos")
