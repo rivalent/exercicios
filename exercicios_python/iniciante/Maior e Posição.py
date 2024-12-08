aac440f66c9becc9d4bc32126267c358a3643ecf
Richard Valente <richardvalente20052005@gmail.com>
2024-12-07 22:32:15 -0300

    exercicios dia 7/12/2024

---
+++ exercicios_python/iniciante/Maior e Posição.py
@@ -0,0 +1,33 @@
+'''
+Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+
+Entrada
+O arquivo de entrada contém 100 números inteiros, positivos e distintos.
+
+Saída
+Apresente o maior valor lido e a posição de entrada, conforme exemplo abaixo.
+
+Exemplo de Entrada	Exemplo de Saída
+2
+113
+45
+34565
+6
+...
+8
+ 
+
+34565
+4
+'''
+
+lista = []
+for valores in range(1, 101):
+    numeros = int(input())
+    lista.append(numeros)
+
+maior = max(lista)
+posicao = lista.index(maior) + 1
+
+print(maior)
+print(posicao)
